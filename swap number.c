06d779825b3d3ccb2af88a57a1694c9032c3808d
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-10-02 19:55:38 +0530

    swap no

---
+++ swap number.c
@@ -0,0 +1,17 @@
+//
+// Created by Sk Naser Hussain on 02-10-2023.
+//
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("ENTER THE VALUE OF a&b:\n");
+    scanf("%d%d",&a,&b);
+    a=c;
+    a=b;
+    b=c;
+    printf("THE RESULT IS a = %d b =%d\n", a,b);
+}
+/***
+* ENTER THE VALUE OF a&b 10 20
+ * THE RESULT IS a = 20 b = 10
+*/
