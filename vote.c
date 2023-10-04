dfbe63e4947c8e52bb0f067783ecd61e7592bb53
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-10-04 19:23:38 +0530

    **

--- vote.c
+++ vote.c
@@ -1,3 +1,28 @@
 //
-// Created by sknas on 03-10-2023.
+// Created by SK NASER HUSSAIN on 03-10-2023.
 //
+#include <stdio.h>
+
+int main() {
+    int age;
+    printf("ENTER THE AGE:\n");
+    scanf("%d",&age);
+    if(age>=18)
+    {
+        printf("PERSON IS ELIGIBLE FOR VOTE!!!\n");
+    }
+    else{
+        printf("PERSON IS NOT ELIGIBLE FOR VOTE!!!\n");
+    }
+    return 0;
+}
+/***
+* ENTER THE AGE:
+21
+PERSON IS ELIGIBLE FOR VOTE!!!
+*/
+/***
+* ENTER THE AGE:
+16
+PERSON IS NOT ELIGIBLE FOR VOTE!!!
+*/
