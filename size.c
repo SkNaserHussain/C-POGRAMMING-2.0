a43f530cb3d547bb77d941173878915e1304a15f
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-10-16 18:40:09 +0530

    f-21

---
+++ size.c
@@ -0,0 +1,13 @@
+//
+// Created by Sk Naser Hussain on 16-10-2023.
+//
+//Write a program in to print the size of a data type in floating point.
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    printf("%f",(float)sizeof(a));
+}
+/***
+* 4.000000
+*/
