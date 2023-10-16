7e250bdd3229d53a118e6c0d9ffbcc8ce4d9b782
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-10-16 18:33:46 +0530

    f-20

---
+++ Right or left shift.c
@@ -0,0 +1,15 @@
+//
+// Created by Sk Naser Hussain on 16-10-2023.
+//
+//Write a program in c to demonstrate the shift right and shift left operator.
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    printf("%d\n",a>>1);
+    printf("%d\n",a<<1);
+}
+/***
+* 5
+20
+*/
