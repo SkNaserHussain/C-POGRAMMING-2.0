4f54eb85468057c3c82e2fb732a11a371b793a5b
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-10-17 16:04:40 +0530

    f-30

---
+++ Bill.c
@@ -0,0 +1,33 @@
+//
+// Created by Sk Naser Hussain on 17-10-2023.
+//
+//Write a c program to calculate the discount amount by taking the price of the product as a input from the user if the discount is greater than 10% but less than 50%,18% GST is to be added to the total bill and if it is more than 50% but less than 100%,2% GST will be applied.
+#include<stdio.h>
+
+int main() {
+    float amt, dis;
+
+    // Input
+    printf("Enter amount: ");
+    scanf("%f", &amt);
+
+    printf("Enter discount: ");
+    scanf("%f", &dis);
+
+    if (dis >= 10 && dis <= 50) {
+        float bill = amt * (dis / 100) + (18.0 / 100.0);
+        printf("Bill = %f\n", bill);
+    } else if (dis > 50 && dis <= 100) {
+        float bill = amt * (dis / 100) + (2.0 / 100.0);
+        printf("Bill = %f\n", bill);
+    } else {
+        printf("ERROR!\n");
+    }
+
+    return 0;
+}
+/***
+* Enter amount: 450
+Enter discount: 15
+Bill = 67.680000
+*/
