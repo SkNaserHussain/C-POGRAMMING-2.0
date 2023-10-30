04edd38cdb54830e31c117a4645a11f17050180d
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-10-30 09:01:33 +0530

    f-41

---
+++ palindrome number.c
@@ -0,0 +1,27 @@
+//
+// Created by Sk Naser Hussain on 30-10-2023.
+//
+//Write a program in c to check whether a number is palindrome or not.
+#include <stdio.h>
+int main()
+{
+    int n, reversed = 0, remainder, original;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    original = n;
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+    if (original == reversed)
+        printf("%d is a palindrome.", original);
+    else
+        printf("%d is not a palindrome.", original);
+}
+/***
+* Enter an integer:50
+ 50 is not a palindrome.
+Process finished with exit code 0
+*/
