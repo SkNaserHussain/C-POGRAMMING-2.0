9a56fb46ee1135c8246577cc15abd6085b7c3045
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-11-09 18:47:07 +0530

    P-44

---
+++ HCF of two numbers.c
@@ -0,0 +1,26 @@
+//
+// Created by Sk Naser Hussain on 09-11-2023.
+//
+////Write a program in c to find the HCF of 2 numbers
+#include<stdio.h>
+
+int main() {
+    int a, b, hcf = 1;
+
+    printf("Enter A and B:");
+    scanf("%d%d", &a, &b);
+
+    for (int i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0) {
+            hcf = i;
+        }
+    }
+
+    printf("The HCF: %d", hcf);
+
+    return 0;
+}
+/***
+* Enter A and B:25 50
+The HCF: 25
+*/
