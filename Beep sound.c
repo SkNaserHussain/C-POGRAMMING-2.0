cd163955bf77fc4e2b2a7f5421a2ffb5f56311c0
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-11-09 13:57:31 +0530

    P-41

---
+++ Beep sound.c
@@ -0,0 +1,10 @@
+//
+// Created by Sk Naser Hussain on 09-11-2023.
+//
+//Write a program in C to make beep sound.
+#include <stdio.h>
+
+int main() {
+    printf("\a");  // Output the ASCII bell character to produce a beep sound
+    return 0;
+}
