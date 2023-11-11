781a5eefe89d5699595595b00b64f1546ba6eaa1
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-11-11 19:25:25 +0530

    P-46

---
+++ Print individual numbers using array.c
@@ -0,0 +1,12 @@
+//
+// Created by Sk Naser Hussain on 11-11-2023.
+//
+//Write a program in C to print individual numbers
+#include<stdio.h>
+int main(){
+    int numbers[4]={110,120,130,140};
+    printf("%d",numbers[2]);
+}
+/***
+* 130
+*/
