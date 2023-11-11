ecb377c183a9f7e6e875de6f62c89e467c76b242
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-11-11 19:17:48 +0530

    P-45

---
+++ Print all the arrays.c
@@ -0,0 +1,17 @@
+//
+// Created by Sk Naser Hussain on 11-11-2023.
+//
+//Write a program in C to print all the arrays.
+#include<stdio.h>
+int main(){
+    int numbers[4]={111,122,133,144};
+    for(int i=0;i<4;i++){
+        printf("%d",numbers[i]);
+    }
+}
+/***
+* 111
+* 122
+* 131
+* 141
+*/
