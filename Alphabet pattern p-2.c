6103ba782687ff51516671b49005770cbd4aee71
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-11-21 19:16:15 +0530

    P-51

---
+++ Alphabet pattern p-2.c
@@ -0,0 +1,35 @@
+//
+// Created by sknas on 21-11-2023.
+//
+/***Write a C program that takes an input 'n' (number of rows) and prints a pattern as follows:
+ *  A
+   AB
+  ABC
+ ABCD
+ABCDE
+ */
+#include<stdio.h>
+
+int main() {
+    int i = 1, j, sp, n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    while (i <= n) {
+        printf("\n");
+        sp = 1;
+        while (sp <= (n - i)) {
+            printf(" ");
+            sp++;
+        }
+        j = 1;
+        char ch = 'A';
+        while (j <= i) {
+            printf("%c", ch);
+            j++;
+            ch++;
+        }
+        i++;
+    }
+    return 0;
+}
+
