dcc46012f4bbbb965182af5dfb8029e503c3f75f
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-11-21 19:06:24 +0530

    P-50

---
+++ increment & decrement.c
@@ -0,0 +1,25 @@
+//
+// Created by sknas on 21-11-2023.
+//
+//Write a C program that demonstrates the use of increment and decrement operators (++ and --)
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    a++;
+    a--;
+    ++a;
+    --a;
+    printf("%d",a);
+}
+/***
+* The output of the provided code will be 10.
+
+Here's the breakdown of the operations:
+
+1.a is initialized to 10.
+2.a++ increments a to 11.
+3.a-- decrements a back to 10.
+4.++a increments a to 11.
+5.--a decrements a back to 10.
+*/
