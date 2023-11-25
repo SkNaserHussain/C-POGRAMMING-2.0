8f3d2193979d1e4b0d33f81b558ce4c8a63e77a6
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-11-25 23:17:21 +0530

    P-66

---
+++ Unique number .c
@@ -0,0 +1,44 @@
+//
+// Created by Sk Naser Hussain on 25-11-2023.
+//
+//Write a c program to check whether a number is unique number or not.
+#include<stdio.h>
+int main()
+{
+    int n, num,r, k = 0, count = 1;
+    int arr[100];
+    printf("Enter a number:");
+    scanf("%d", &n);
+    num = n;
+    while (n > 0)
+    {
+        r = n % 10;
+        arr[k] = r;
+        n = n / 10;
+        k++;
+    }
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = i + 1; j < k; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count = 0;
+            }
+        }
+    }
+    if (count == 1)
+    {
+        printf("%d is unique.", num);
+    }
+    else
+    {
+        printf("%d is not unique.", num);
+    }
+}
+/***
+* Enter a number:190
+190 is unique.
+ Enter a number:101
+101 is not unique.
+*/
