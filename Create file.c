dba3347499d264c7ed2156a951184881c42e1092
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-12-01 11:16:48 +0530

    P-84

---
+++ Create file.c
@@ -0,0 +1,16 @@
+//
+// Created by Sk Naser Hussain on 01-12-2023.
+//
+//write a Program in C to create a file
+#include<stdio.h>
+
+int main() {
+    char fname[100];
+    printf("Enter file name to be created >> ");
+    gets(fname);
+
+    FILE *fp;
+    fp=fopen(fname, "w");
+    printf("File successfully created !!! ");
+    fclose(fp);
+}
