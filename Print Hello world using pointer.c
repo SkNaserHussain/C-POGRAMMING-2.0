0e15cf17ffbf08e27bbd2ddbbbdf437ad72e3a86
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-12-10 14:20:38 +0530

    P-90

---
+++ Print Hello world using pointer.c
@@ -0,0 +1,10 @@
+//
+// Created by sknas on 10-12-2023.
+//
+//Write a program to print Hello World, Using Pointers.
+#include<stdio.h>
+int main(){
+    char *a="Hello World!!";
+    printf("%s",a);
+    return 0;
+}
