17cbc3b81b58c0e6a14bb276dc3a08909599eacd
SkNaserHussain <sknaserhussain@gmail.com>
2023-12-31 17:50:42 +0530

    P-101

---
+++ Print Hello world n times.c
@@ -0,0 +1,11 @@
+//
+// Created by Sk Naser Hussain on 31-12-2023.
+//
+//print hello world!! n times in c without stop until memory become full.
+#include<stdio.h>
+int main(){
+    while(1){
+        printf("HELLO WORLD!!\n");
+    }
+    return 0;
+}
